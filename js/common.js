fcabc3f14e4639e575c08b3f9e13f6143a78f91e
greenlilys <1635382999@qq.com>
2019-01-04 10:01:43 +0800

    新增

---
+++ js/common.js
@@ -0,0 +1,29 @@
+$(function(){
+    
+    $('.tanchuang li').click(function(){           
+        $(this).siblings().find('a').removeClass('activs');
+        $(this).find('a').addClass('activs');
+    })
+    var isMobile = (document.documentElement.clientWidth || document.body.clientWidth) > 768;
+    if(isMobile){
+        $('.ours').mouseover(function(){
+            $('.tanchuang').css('display','block')
+        }).mouseout(function(){
+            $('.tanchuang').css('display','none')
+        })      
+        
+    }
+    
+
+    if(navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)){//移动设备清除导航栏右内边距
+        $('.guanwo').click(function(){						
+            if($('.tanchuang').css('display') == 'block'){
+                $('.tanchuang').hide(300);
+            }else{
+                $('.tanchuang').show(300);
+            }
+        }) 
+            
+    } 
+    
+})
